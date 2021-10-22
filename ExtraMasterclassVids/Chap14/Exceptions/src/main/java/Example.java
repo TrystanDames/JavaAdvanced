3b25025c1d96d69844f409daee3bb147f8720977
Trystan <trystandames08@gmail.com>
2021-10-22 14:24:17 +0200

    24th Commit

--- ExtraMasterclassVids/Chap14/Exceptions/src/main/java/Example.java
+++ ExtraMasterclassVids/Chap14/Exceptions/src/main/java/Example.java
@@ -5,24 +5,27 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch(ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shutting down");
+        }
     }
 
     private static int divide() {
         int x, y;
-        try {
-            x = getInt();
-            y = getInt();
-        } catch(NoSuchElementException e) {
-            throw new ArithmeticException("no suitable input");
-        }
+//        try {
+        x = getInt();
+        y = getInt();
         System.out.println("x is " + x + ", y is " + y);
-        try {
-            return x / y;
-        } catch(ArithmeticException e) {
-            throw new ArithmeticException("attempt to divide by zero");
-        }
+        return x / y;
+//        } catch(NoSuchElementException e) {
+//            throw new ArithmeticException("no suitable input");
+//        } catch(ArithmeticException e) {
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
     }
 
     private static int getInt() {
@@ -32,7 +35,6 @@ public class Example {
             try {
                 return s.nextInt();
             } catch(InputMismatchException e) {
-                // go round again.  Read past the end of line in the input first
                 s.nextLine();
                 System.out.println("Please enter a number using only the digits 0 to 9 ");
             }
