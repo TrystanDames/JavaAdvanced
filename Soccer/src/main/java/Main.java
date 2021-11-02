f9579a7801071f7c71588ceaa1d92a6a01c43af5
Trystan <trystandames08@gmail.com>
2021-11-02 13:59:32 +0200

    30th Commit

--- Soccer/src/main/java/Main.java
+++ Soccer/src/main/java/Main.java
@@ -33,13 +33,13 @@ public class Main {
             team2 = l2[0];
             score2 = l2[1];
 
-            l3 = sides[0].split(" ");
-            team3 = l3[0];
-            score3 = l3[1];
-
-            l4 = sides[1].split(" ");
-            team4 = l4[0];
-            score4 = l4[1];
+//            l3 = sides[0].split(" ");
+//            team3 = l3[0];
+//            score3 = l3[1];
+//
+//            l4 = sides[1].split(" ");
+//            team4 = l4[0];
+//            score4 = l4[1];
 //
 //            l5 = sides[4].split(" ");
 //            team5 = l5[0];
@@ -51,8 +51,8 @@ public class Main {
 
             int teamScore1 = Integer.parseInt(score1);
             int teamScore2 = Integer.parseInt(score2);
-            int teamScore3 = Integer.parseInt(score3);
-            int teamScore4 = Integer.parseInt(score4);
+//            int teamScore3 = Integer.parseInt(score3);
+//            int teamScore4 = Integer.parseInt(score4);
 //            int teamScore5 = Integer.parseInt(score5);
 //            int teamScore6 = Integer.parseInt(score6);
 
@@ -66,61 +66,62 @@ public class Main {
                 scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
             }
 
-            if (teamScore3 > teamScore4) {
-                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
-            } else if (teamScore3 < teamScore4) {
-                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
-            } else {
-                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
-                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
-            }
+//            if (teamScore3 > teamScore4) {
+//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
+//            } else if (teamScore3 < teamScore4) {
+//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
+//            } else {
+//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
+//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
+//            }
+////
+//            if (teamScore2 > teamScore4) {
+//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 3);
+//            } else if (teamScore2 < teamScore4) {
+//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
+//            } else {
+//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
+//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
+//            }
 //
-            if (teamScore2 > teamScore4) {
-                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 3);
-            } else if (teamScore2 < teamScore4) {
-                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
-            } else {
-                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
-                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
-            }
-
-            if (teamScore3 > teamScore2) {
-                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
-            } else if (teamScore1 < teamScore2) {
-                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 3);
-            } else {
-                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
-                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
-            }
-
-            if (teamScore1 > teamScore3) {
-                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 3);
-            } else if (teamScore1 < teamScore3) {
-                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
-            } else {
-                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 1);
-                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
-            }
-
-            if (teamScore4 > teamScore1) {
-                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
-            } else if (teamScore3 < teamScore1) {
-                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 3);
-            } else {
-                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 1);
-                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
-            }
+//            if (teamScore3 > teamScore2) {
+//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
+//            } else if (teamScore1 < teamScore2) {
+//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 3);
+//            } else {
+//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
+//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
+//            }
+//
+//            if (teamScore1 > teamScore3) {
+//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 3);
+//            } else if (teamScore1 < teamScore3) {
+//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
+//            } else {
+//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 1);
+//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
+//            }
+//
+//            if (teamScore4 > teamScore1) {
+//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
+//            } else if (teamScore3 < teamScore1) {
+//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 3);
+//            } else {
+//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 1);
+//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
+//            }
         }
 
-        LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
+            LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
 
-        scoreboard.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            scoreboard.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
 //                .sorted(Map.Entry.comparingByKey())
-                .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+                    .forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
 
-        System.out.println("Rankings: " + reverseSortedMap);
+            System.out.println("Rankings: " + reverseSortedMap);
+
     }
 }
 
