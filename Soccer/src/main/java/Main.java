ab147d040af83fdf90c96859343730e873e6366f
Trystan <trystandames08@gmail.com>
2021-11-04 08:40:50 +0200

    32nd Commit

--- Soccer/src/main/java/Main.java
+++ Soccer/src/main/java/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\User\\Desktop\\JavaAdvanced\\Soccer\\src\\main\\resources\\result.txt");
         Scanner sc = null;
-        String match, team1, team2, team3, team4, score1, score2, score3, score4;
+        String match, team1, team2, score1, score2;
         int leagueTotal = 0;
         TreeMap<String, Integer> scoreboard = new TreeMap<>();
 
-        String[] sides, l1, l2, l3, l4;
+        String[] sides, l1, l2;
         try {
             sc = new Scanner(file);
         } catch (FileNotFoundException e) {
@@ -21,7 +21,7 @@ public class Main {
 
         while (sc.hasNextLine()) {
 
-            Map<String, Integer> map = new TreeMap<>();
+//            Map<String, Integer> map = new TreeMap<>();
             leagueTotal = 0;
             match = sc.nextLine();
             sides = match.split(",");
@@ -33,28 +33,10 @@ public class Main {
             team2 = l2[0];
             score2 = l2[1];
 
-//            l3 = sides[0].split(" ");
-//            team3 = l3[0];
-//            score3 = l3[1];
-//
-//            l4 = sides[1].split(" ");
-//            team4 = l4[0];
-//            score4 = l4[1];
-//
-//            l5 = sides[4].split(" ");
-//            team5 = l5[0];
-//            score5 = l5[1];
-//
-//            l6 = sides[5].split(" ");
-//            team6 = l6[0];
-//            score6 = l6[1];
 
             int teamScore1 = Integer.parseInt(score1);
             int teamScore2 = Integer.parseInt(score2);
-//            int teamScore3 = Integer.parseInt(score3);
-//            int teamScore4 = Integer.parseInt(score4);
-//            int teamScore5 = Integer.parseInt(score5);
-//            int teamScore6 = Integer.parseInt(score6);
+
 
 
             if (teamScore1 > teamScore2) {
@@ -66,50 +48,6 @@ public class Main {
                 scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
             }
 
-//            if (teamScore3 > teamScore4) {
-//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
-//            } else if (teamScore3 < teamScore4) {
-//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
-//            } else {
-//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
-//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
-//            }
-////
-//            if (teamScore2 > teamScore4) {
-//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 3);
-//            } else if (teamScore2 < teamScore4) {
-//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
-//            } else {
-//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
-//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
-//            }
-//
-//            if (teamScore3 > teamScore2) {
-//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
-//            } else if (teamScore1 < teamScore2) {
-//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 3);
-//            } else {
-//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
-//                scoreboard.put(team2, scoreboard.getOrDefault(team2, leagueTotal) + 1);
-//            }
-//
-//            if (teamScore1 > teamScore3) {
-//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 3);
-//            } else if (teamScore1 < teamScore3) {
-//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 3);
-//            } else {
-//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 1);
-//                scoreboard.put(team3, scoreboard.getOrDefault(team3, leagueTotal) + 1);
-//            }
-//
-//            if (teamScore4 > teamScore1) {
-//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 3);
-//            } else if (teamScore3 < teamScore1) {
-//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 3);
-//            } else {
-//                scoreboard.put(team1, scoreboard.getOrDefault(team1, leagueTotal) + 1);
-//                scoreboard.put(team4, scoreboard.getOrDefault(team4, leagueTotal) + 1);
-//            }
         }
 
             LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
